d205b7a9e2dc9bca796daa956437b372ecbddfc6
MiaoMint <MiaoMint@users.noreply.github.com>
2023-07-23 18:09:16 +0000

    deploy: 275221d88b7a1281192a64149ef2079c1c8327f5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "49b99865b3e59c8c2aa52391bea2a489",
-"/": "49b99865b3e59c8c2aa52391bea2a489",
+const RESOURCES = {"index.html": "834f06847bfff9ed81de189e80eca895",
+"/": "834f06847bfff9ed81de189e80eca895",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "cb971fd0adebd388c506b24dbc9324ea",
