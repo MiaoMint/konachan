adff254577b3223b97a93a6e7d2356ff144101a5
MiaoMint <44718819+MiaoMint@users.noreply.github.com>
2023-07-24 02:05:53 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,11 @@ const proxy = createProxyMiddleware({
   pathRewrite: {
     "^/api": "",
   },
+  headers: {
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183",
+    referer: "https://konachan.net",
+  },
 });
 
 // Expose the proxy on the "/api/*" endpoint.
